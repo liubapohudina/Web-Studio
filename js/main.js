8a8acfb9e56b9496ddacb8f9b24402f6dd64063c
Liubov <drogaltseva92@gmail.com>
2023-12-11 13:17:31 +0100

    add scroll

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ function handleClick(event) {
   });
 }
 
+ 
 
 
 
--- js/modal.js
+++ js/modal.js
@@ -12,4 +12,27 @@
       refs.modal.classList.toggle("is-hidden");
       document.body.classList.toggle("modal-open");
     }
-  })();
+})();
+/*------------------------scroll top---------------------*/
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+}
+
+
+function scrollToTop() {
+  //const scrollToTopButton = document.getElementById('myBtn'); 
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+const scrollToTopButton = document.getElementById('myBtn');
+console.log(scrollToTopButton)
+scrollToTopButton.addEventListener('click', scrollToTop);
